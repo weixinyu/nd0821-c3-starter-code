836be369f990342d7f52d69c80e4eb8026a80b35
weixinyu <weixinyu2012@126.com>
2023-05-15 02:26:14 +0000

    add the training part

--- starter/starter/ml/model.py
+++ starter/starter/ml/model.py
@@ -1,5 +1,5 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
-
+from sklearn.ensemble import RandomForestClassifier
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -18,7 +18,10 @@ def train_model(X_train, y_train):
         Trained machine learning model.
     """
 
-    pass
+    rfc = RandomForestClassifier(random_state=0);
+    rfc.fit(X_train, y_train)
+    
+    return rfc
 
 
 def compute_model_metrics(y, preds):
@@ -48,7 +51,7 @@ def inference(model, X):
 
     Inputs
     ------
-    model : ???
+    model : RandomForestClassifier
         Trained machine learning model.
     X : np.array
         Data used for prediction.
@@ -57,4 +60,6 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    pass
+    preds = model.predict(X)
+    
+    return preds
--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,11 +1,17 @@
 # Script to train machine learning model.
-
+import pandas as pd
+import os
+import pickle
 from sklearn.model_selection import train_test_split
 
+
+from ml.data import process_data
+from ml.model import train_model
+from ml.model import compute_model_metrics
 # Add the necessary imports for the starter code.
 
 # Add code to load in the data.
-
+data = pd.read_csv(os.getcwd() + '/data/census_clean.csv')    
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
 
@@ -24,5 +30,13 @@ X_train, y_train, encoder, lb = process_data(
 )
 
 # Proces the test data with the process_data function.
+X_test, y_test, encoder, lb = process_data(
+    train, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb
+)
 
 # Train and save a model.
+model = train_model(X_train, y_train)
+
+with open('model/model.pickle', 'wb') as f:
+    pickle.dump(model, f)
+
