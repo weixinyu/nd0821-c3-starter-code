31e6b08270b072ccf9c5a320d76e800e7a69ee24
weixinyu <weixinyu2012@126.com>
2023-05-18 11:58:01 +0000

    update test

--- starter/test_main.py
+++ starter/test_main.py
@@ -16,4 +16,4 @@ def test_predict():
     data = '{"age": 65, "workclass": "Private", "fnlgt": 153522, "education": "HS-grad", "education-num": 9, "marital-status": "Widowed", "occupation": "Other-service", "relationship": "Unmarried", "race": "White", "sex": "Female", "capital-gain": 0, "capital-loss": 0, "hours-per-week": 17, "native-country": "United-States"}'
     r = client.post("/predict", data=data)
     print(r.json())
-    assert r.json()["result"] == 42
+    assert r.json()["result"] == '<=50K'
