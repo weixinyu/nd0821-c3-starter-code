6d82bf3fcd897e33c89328938ac102d067300aaa
weixinyu <weixinyu2012@126.com>
2023-05-18 12:11:18 +0000

    change test

--- starter/test_main.py
+++ starter/test_main.py
@@ -12,8 +12,16 @@ def test_say_hello():
     assert r.status_code == 200
     assert r.json() == {"greeting": "Hello World!"}
     
-def test_predict():
+def test_predict_1():
     data = '{"age": 65, "workclass": "Private", "fnlgt": 153522, "education": "HS-grad", "education-num": 9, "marital-status": "Widowed", "occupation": "Other-service", "relationship": "Unmarried", "race": "White", "sex": "Female", "capital-gain": 0, "capital-loss": 0, "hours-per-week": 17, "native-country": "United-States"}'
     r = client.post("/predict", data=data)
+    assert r.status_code == 200
     print(r.json())
     assert r.json()["result"] == '<=50K'
+    
+def test_predict_2():
+    data = '{"age": 47, "workclass": "Self-emp-not-inc", "fnlgt": 192755, "education": "HS-grad", "education-num": 9, "marital-status": "Married-civ-spouse", "occupation": "Machine-op-inspct", "relationship": "Wife", "race": "White", "sex": "Female", "capital-gain": 0, "capital-loss": 0, "hours-per-week": 20, "native-country": "United-States"}'
+    r = client.post("/predict", data=data)
+    assert r.status_code == 200
+    print(r.json())
+    #assert r.json()["result"] == '<=50K'
